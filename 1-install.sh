e4c094438eaca4bf198deb78ad0c0fb55e98ad9d
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2023-10-21 07:41:05 +0100

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -172,7 +172,7 @@ while true; do
             if [ -d ~/wallpaper/ ]; then
                 echo "wallpaper folder already exists."
             else
-                git clone https://gitlab.com/stephan-raabe/wallpaper.git ~/wallpaper
+                git clone https://gitlab.com/Sproggy/wallpaper.git ~/wallpaper
                 echo "wallpaper installed."
             fi
             echo "Wallpaper installed."
