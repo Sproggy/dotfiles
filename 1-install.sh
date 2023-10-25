9203e3d57bfef9bfc8ec954d943f7ed445137118
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2023-10-25 08:50:26 +0000

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -129,19 +129,6 @@ packagesYay=(
     "bibata-cursor-theme" 
     "trizen"
     "sddm-sugar-dark"
-    "gvfs-afc"
-    "gvfs-goa"
-    "gvfs-gphoto2"
-    "gvfs-mtp"
-    "gvfs-nfs"
-    "gvfs-smb"
-    "thunar-archive-plugin"
-    "thunar-custom-acions"
-    "thunar-media-tags-plugin"
-    "thunar-shares-plugin"
-    "thunar-vcs-plugin"
-    "thunar-volman"
-    "gnome-disk-untility"
 );
   
 # ------------------------------------------------------
