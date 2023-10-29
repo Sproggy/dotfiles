b200d85c0c487bdcbe7f71f53d66321ae42e0011
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2023-10-29 09:26:06 +0000

    Create Volume.sh

---
+++ scripts/Volume.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Get Volume
+get_volume() {
+	volume=$(pamixer --get-volume)
+	echo "$volume"
+}
+
+# Notify
+notify_user() {
+#	notify-send -h string:x-canonical-private-synchronous:sys-notify -u normal -i "Volume : $(get_volume) %"
+	notify-send -h int:value:$(get_volume) -h "string:x-dunst-stack-tag:volume_notif" -u low -i "Volume : $(get_volume) %"
+
+}
+
+# Increase Volume
+inc_volume() {
+	pamixer -i 5 && notify_user
+}
+
+# Decrease Volume
+dec_volume() {
+	pamixer -d 5 && notify_user
+}
+
+# Toggle Mute
+toggle_mute() {
+	if [ "$(pamixer --get-mute)" == "false" ]; then
+		pamixer -m && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "Volume Switched OFF"
+	elif [ "$(pamixer --get-mute)" == "true" ]; then
+		pamixer -u && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "Volume Switched ON"
+	fi
+}
+
+# Toggle Mic
+toggle_mic() {
+	if [ "$(pamixer --default-source --get-mute)" == "false" ]; then
+		pamixer --default-source -m && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "Microphone Switched OFF"
+	elif [ "$(pamixer --default-source --get-mute)" == "true" ]; then
+		pamixer -u --default-source u && notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "Microphone Switched ON"
+	fi
+}
+
+# Notify
+notify_mic_user() {
+#	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "Mic-Level : $(pamixer --default-source --get-volume) %"
+	notify-send -h int:value:$(pamixer --default-source --get-volume) -h "string:x-dunst-stack-tag:volume_notif" -u low -i "Mic-Level : $(pamixer --default-source --get-volume) %"
+}
+
+# Increase MIC Volume
+inc_mic_volume() {
+	pamixer --default-source -i 5 && notify_mic_user
+}
+
+# Decrease MIC Volume
+dec_mic_volume() {
+	pamixer --default-source -d 5 && notify_mic_user
+}
+
+# Execute accordingly
+if [[ "$1" == "--get" ]]; then
+	get_volume
+elif [[ "$1" == "--inc" ]]; then
+	inc_volume
+elif [[ "$1" == "--dec" ]]; then
+	dec_volume
+elif [[ "$1" == "--toggle" ]]; then
+	toggle_mute
+elif [[ "$1" == "--toggle-mic" ]]; then
+	toggle_mic
+elif [[ "$1" == "--get-icon" ]]; then
+	get_icon
+elif [[ "$1" == "--get-mic-icon" ]]; then
+	get_mic_icon
+elif [[ "$1" == "--mic-inc" ]]; then
+	inc_mic_volume
+elif [[ "$1" == "--mic-dec" ]]; then
+	dec_mic_volume
+else
+	get_volume
+fi
