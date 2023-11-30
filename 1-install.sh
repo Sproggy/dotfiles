d490d88655d27c4794d0c1644c5a147d11df0aa8
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2023-11-30 22:58:42 +0000

    updated intaller to all in one

--- 1-install.sh
+++ 1-install.sh
@@ -100,6 +100,7 @@ while true; do
 done
 echo ""
 echo ""
+sleep 3
 echo ""
 echo "
 #########################################################
@@ -123,6 +124,11 @@ case $GRAPHICSCARD in
 2)
   sudo pacman -S --noconfirm xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau;;
 3)
+  sudo sed -i 's/GRUB_CMDLINE_LINUX="rootfstype=ext4"/GRUB_CMDLINE_LINUX="rootfstype=ext4 nvidia_drm.modeset=1 rd.driver.blacklist=nouveau modprob.blacklist=nouveau"/' /etc/default/grub
+  sudo grub-mkconfig -o /boot/grub/grub.cfg
+  sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+  sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+  echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
   sudo pacman -S --noconfirm nvidia-dkms nvidia-utils nvidia-settings qt5-wayland qt5ct qt6-wayland qt6ct libva && yay --noconfirm -S libva-nvidia-driver-git;;
 *)
   sudo pacman -S --noconfirm xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau;;
@@ -138,7 +144,7 @@ echo "
 #########################################################
 
 "
-
+sleep 3
 while true; do
     read -p "DO YOU WANT TO INSTALL THE CORE APPS NOW? (Yy/Nn): " yn
     case $yn in
@@ -375,6 +381,7 @@ echo "
 #                 Wallpapers Installed                  #
 #                                                       #
 #########################################################
+
 "
 echo ""
 echo "
@@ -487,7 +494,7 @@ echo "
 #             Installing required Packages              #
 #                                                       #
 #########################################################
-echo ""
+
 "
 echo ""
 echo "-> Install main packages"
@@ -629,7 +636,6 @@ echo "
 "
 sudo systemctl start bluetooth
 sudo systemctl enable bluetooth
-sudo grub-mkconfig -o /boot/grub/grub.cfg
 echo ""
 echo ""
 echo "
@@ -646,6 +652,151 @@ echo "If you installed an NVIDIA Graphics Card please follow the instructions in
 echo "nvidia.conf file located ~/dotfiles/hypr/conf/nvidia.conf"
 echo ""
 echo ""
+echo ""
+sleep 5
+echo "       ██            ██     ████ ██  ██                 "
+echo "      ░██           ░██    ░██░ ░░  ░██                 "
+echo "      ░██  ██████  ██████ ██████ ██ ░██  █████   ██████ "
+echo "   ██████ ██░░░░██░░░██░ ░░░██░ ░██ ░██ ██░░░██ ██░░░░  "
+echo "  ██░░░██░██   ░██  ░██    ░██  ░██ ░██░███████░░█████  "
+echo " ░██  ░██░██   ░██  ░██    ░██  ░██ ░██░██░░░░  ░░░░░██ "
+echo " ░░██████░░██████   ░░██   ░██  ░██ ███░░██████ ██████  "
+echo "  ░░░░░░  ░░░░░░     ░░    ░░   ░░ ░░░  ░░░░░░ ░░░░░░   "
+echo ""
+echo " by Sproggy (Corrie Tilcock) (2023) "
+echo " ------------------------------------------------------------------- "
+echo ""
+echo "The script will ask for permission to remove existing directories and files from ~/.config/"
+echo "Symbolic links will then be created from ~/dotfiles into your ~/.config/ directory."
+echo "But you can decide to keep your personal versions by answering with No (Nn)."
+echo ""
+echo ""
+echo "
+#########################################################
+#                                                       #
+#              Confirm dotfile files Install            #
+#                                                       #
+#########################################################
+
+"
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+        break;;
+        [Nn]* ) 
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo ""
+echo "
+#########################################################
+#                                                       #
+#             Check .config directory exists            #
+#                                                       #
+#########################################################
+
+"
+echo ""
+echo "-> Check if .config folder exists"
+
+if [ -d ~/.config ]; then
+    echo ".config folder already exists."
+else
+    mkdir ~/.config
+    echo ".config folder created."
+fi
+echo ""
+echo "
+#########################################################
+#                                                       #
+#                 Create Symbolic Links                 #
+#                                                       #
+#########################################################
+
+"
+# name symlink source target
+echo ""
+echo ""
+echo "-------------------------------------"
+echo "-> Install general dotfiles"
+echo "-------------------------------------"
+echo ""
+echo ""
+_installSymLink alacritty ~/.config/alacritty ~/dotfiles/alacritty/ ~/.config
+_installSymLink ranger ~/.config/ranger ~/dotfiles/ranger/ ~/.config
+_installSymLink vim ~/.config/vim ~/dotfiles/vim/ ~/.config
+_installSymLink nvim ~/.config/nvim ~/dotfiles/nvim/ ~/.config
+_installSymLink starship ~/.config/starship.toml ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+_installSymLink rofi ~/.config/rofi ~/dotfiles/rofi/ ~/.config
+_installSymLink dunst ~/.config/dunst ~/dotfiles/dunst/ ~/.config
+_installSymLink wal ~/.config/wal ~/dotfiles/wal/ ~/.config
+echo ""
+echo "
+#########################################################
+#                                                       #
+#                  Re-Initiating Pywal                  #
+#                                                       #
+#########################################################
+
+"
+echo ""
+wal -i wallpapers/
+echo "Pywal templates initiated!"
+echo ""
+echo ""
+echo "
+#########################################################
+#                                                       #
+#                    Pywal Initiated                    #
+#                                                       #
+#########################################################
+
+"
+echo "-------------------------------------"
+echo "-> Install GTK dotfiles"
+echo "-------------------------------------"
+echo ""
+echo ""
+_installSymLink .gtkrc-2.0 ~/.gtkrc-2.0 ~/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+_installSymLink gtk-3.0 ~/.config/gtk-3.0 ~/dotfiles/gtk/gtk-3.0/ ~/.config/
+_installSymLink gtk-4.0 ~/.config/gtk-4.0 ~/dotfiles/gtk/gtk-4.0/ ~/.config/
+_installSymLink themes ~/.local/share/themes ~/dotfiles/themes ~/.local/share/
+echo ""
+echo "-------------------------------------"
+echo "-> Install Thunar dotfiles"
+echo "-------------------------------------"
+echo ""
+echo ""
+_installSymLink xfce-perchannel-xml ~/.config/xfce4/xfconf/xfce-perchannel-xml ~/dotfiles/xfce-perchannel-xml ~/.config/xfce4/xfconf/
+_installSymLink Thunar ~/.config/Thunar ~/dotfiles/Thunar ~/.config/
+echo ""
+echo "-------------------------------------"
+echo "-> Install Qtile dotfiles"
+echo "-------------------------------------"
+echo ""
+echo ""
+_installSymLink qtile ~/.config/qtile ~/dotfiles/qtile/ ~/.config
+_installSymLink polybar ~/.config/polybar ~/dotfiles/polybar/ ~/.config
+_installSymLink picom ~/.config/picom ~/dotfiles/picom/ ~/.config
+_installSymLink .xinitrc ~/.xinitrc ~/dotfiles/qtile/.xinitrc ~/.xinitrc
+echo ""
+echo "-------------------------------------"
+echo "-> Install Hyprland dotfiles"
+echo "-------------------------------------"
+echo ""
+echo ""
+_installSymLink hypr ~/.config/hypr ~/dotfiles/hypr/ ~/.config
+_installSymLink waybar ~/.config/waybar ~/dotfiles/waybar/ ~/.config
+_installSymLink swaylock ~/.config/swaylock ~/dotfiles/swaylock/ ~/.config
+_installSymLink wlogout ~/.config/wlogout ~/dotfiles/wlogout/ ~/.config
+_installSymLink swappy ~/.config/swappy ~/dotfiles/swappy/ ~/.config
+echo ""
+echo ""
 echo "DONE!"
 echo "NEXT: Update the keyboard layout and screen resolution in ~/dotfiles/hypr/hyprland.conf"
-echo "Now proceed with running 2-dotfiles.sh"
+echo "Now proceed with rebooting your system and Enjoy!!!"
+echo ""
