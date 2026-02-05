041e765d07a230e9631fab2f4082b35f59348639
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2026-02-05 13:17:16 +0000

    refresh 2026

--- hyprlogout/logout.sh
+++ hyprlogout/logout.sh
@@ -21,18 +21,20 @@
 # by Sproggy (Corrie Tilcock) (2026)
 # ----------------------------------------------------- 
 
-chosen=$(echo -e "Lock\nLogout\nShutdown\nReboot\nSuspend\nHibernate\n[Close Menu]" | rofi -dmenu -config ~/dotfiles/rofi/config-power.rasi -show-icons /home/test/dotfiles/hyprlogout/icons/ -p "Power")
+chosen=$(echo -e " Lock\n Logout\n Hibernate\n Suspend\n Reboot\n Shutdown\n[Close Menu]" | rofi -dmenu -config ~/dotfiles/rofi/config-power.rasi -p "Power")
 
-if [[ $chosen = "Lock" ]]; then
+#chosen=$(echo -e "Lock\nLogout\nReboot\nSuspend\nHibernate\nShutdown\n[Close Menu]" | rofi -dmenu -config ~/dotfiles/rofi/config-power.rasi -p "Power")
+
+if [[ $chosen = " Lock" ]]; then
     swaylock
-elif [[ $chosen = "Logout" ]]; then
+elif [[ $chosen = " Logout" ]]; then
     hyprctl dispatch exit
-elif [[ $chosen = "Reboot" ]]; then
+elif [[ $chosen = " Reboot" ]]; then
     systemctl reboot
-elif [[ $chosen = "Shutdown" ]]; then
+elif [[ $chosen = " Shutdown" ]]; then
     systemctl poweroff
-elif [[ $chosen = "Suspend" ]]; then
+elif [[ $chosen = " Suspend" ]]; then
     systemctl suspend
-elif [[ $chosen = "Hibernate" ]]; then
+elif [[ $chosen = " Hibernate" ]]; then
     systemctl hibernate
 fi
