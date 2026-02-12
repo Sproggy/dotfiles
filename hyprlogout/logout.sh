da12b298e1c105faa7253b812a130f9d7c04a9f2
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2026-02-12 10:39:41 +0000

    refresh 2026

--- hyprlogout/logout.sh
+++ hyprlogout/logout.sh
@@ -20,19 +20,98 @@
 #                                                                        
 # by Sproggy (Corrie Tilcock) (2026)
 # ----------------------------------------------------- 
+# Current Theme
+dir="$HOME/.config/rofi"
+theme='config-power'
 
-chosen=$(echo -e " Lock\n Logout\n Hibernate\n Reboot\n Shutdown\n Suspend" | rofi -dmenu -config ~/dotfiles/rofi/config-power.rasi -p "Power")
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+#host=`hostnamectl --static`
 
-if [[ $chosen = " Lock" ]]; then
-    swaylock
-elif [[ $chosen = " Logout" ]]; then
-    hyprctl dispatch exit
-elif [[ $chosen = " Reboot" ]]; then
-    systemctl reboot
-elif [[ $chosen = " Shutdown" ]]; then
-    systemctl poweroff
-elif [[ $chosen = " Suspend" ]]; then
-    systemctl suspend
-elif [[ $chosen = " Hibernate" ]]; then
-    systemctl hibernate
-fi
+# Options
+shutdown=''
+reboot=''
+lock=''
+suspend=''
+hibernate=''
+logout=''
+yes=''
+no=''
+
+# Rofi CMD
+rofi_cmd() {
+    rofi -dmenu \
+        -p "Uptime: $uptime" \
+        -mesg "Uptime: $uptime" \
+        -theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 400px;}' \
+         -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+         -theme-str 'listview {columns: 2; lines: 1;}' \
+         -theme-str 'element-text {horizontal-align: 0.5;}' \
+         -theme-str 'textbox {horizontal-align: 0.5;}' \
+         -dmenu \
+         -p 'Confirmation' \
+         -mesg 'Are you Sure?' \
+         -theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+    echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+    echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--suspend' ]]; then
+            mpc -q pause
+            amixer set Master mute
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            hyprctl dispatch exit
+        elif [[ $1 == '--hibernate' ]]; then
+            systemctl hibernate
+        elif [[ $1 == '--lock' ]]; then
+            swaylock
+        fi
+    else
+        exit 0
+    fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+        run_cmd --shutdown
+        ;;
+    $reboot)
+        run_cmd --reboot
+        ;;
+    $lock)
+        run_cmd --lock
+        ;;
+    $suspend)
+        run_cmd --suspend
+        ;;
+    $suspend)
+        run_cmd --hibernate
+        ;;
+    $logout)
+        run_cmd --logout
+        ;;
+esac
