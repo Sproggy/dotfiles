a45ecf66db2739692fb7a54dd5915fb4e3f70a4d
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2026-02-27 13:14:44 +0000

    Update system.sh

--- hypr/packages/system.sh
+++ hypr/packages/system.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 figlet -f 3d "System"
 echo " System Packages "
-sudo pacman -S sddm blueman papirus-icon-theme pacman-contrib awesome-terminal-fonts ttf-font-awesome ttf-fira-sans ttf-fira-code ttf-firacode-nerd exa python-pip python-psutil python-rich python-click xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk os-prober polkit-gnome gnome-keyring --noconfirm
+sudo pacman -S sddm blueman papirus-icon-theme pacman-contrib awesome-terminal-fonts ttf-font-awesome ttf-fira-sans ttf-fira-code ttf-firacode-nerd exa python-pip python-psutil python-rich python-click xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk os-prober polkit-gnome gnome-keyring pcp pcp-gui --noconfirm
+sudo pacman -S $(pacman -Ssq 'pcp-pmda-*') --noconfirm
 echo ""
 yay -S bibata-cursor-theme waypaper trizen sublime-text-4 sddm-theme-sugar-candy-git pacseek --noconfirm
 echo ""
