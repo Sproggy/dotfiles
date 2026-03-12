bd3c7597d6cfc51e3d3a2039bd7ebd16fe7bc1ea
Corrie Tilcock <66495933+Sproggy@users.noreply.github.com>
2026-03-12 09:17:10 +0000

    Update systemtools.sh

--- hypr/packages/systemtools.sh
+++ hypr/packages/systemtools.sh
@@ -3,5 +3,5 @@ figlet -f 3d "Sys Tools"
 echo " System Tools "
 sudo pacman -S timeshift file-roller gparted xfce4-power-manager rofi dunst cockpit --noconfirm
 echo ""
-yay -S gnome-disk-utility octopi --noconfirm
+yay -S gnome-disk-utility --noconfirm
 echo ""
