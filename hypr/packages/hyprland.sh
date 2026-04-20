e268cdb7d96c8765a8d9ad457fc351270c9bb289
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-04-20 12:51:35 +0100

    Update hyprland.sh

--- hypr/packages/hyprland.sh
+++ hypr/packages/hyprland.sh
@@ -3,6 +3,6 @@ figlet -f 3d "Hyprland"
 echo " Hyprland "
 sudo pacman -S hyprland xdg-desktop-portal-wlr waybar swayidle swappy cliphist xorg-xhost nwg-look mission-center curl imagemagick jq bc brightnessctl playerctl libadwaita gtk-layer-shell python python-pip python-virtualenv python-gobject gtk4 wob --noconfirm
 
-yay -S awww swaylock-effects gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb p7zip-full-bin unzip unrar --noconfirm
+yay -S awww swaylock-effects gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb unzip unrar --noconfirm
 
 
