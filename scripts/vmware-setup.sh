3a1845eafd7b12c954089ed1ec3a0b6a7ffc371a
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-04-20 13:23:38 +0100

    Update vmware-setup.sh

--- scripts/vmware-setup.sh
+++ scripts/vmware-setup.sh
@@ -6,7 +6,42 @@ lscpu | grep Virtualization
 sleep 2
 echo ""
 echo " Check If Virtualisation is enabled "
-sudo pacman -S --needed --noconfirm linux-headers
+echo "
+#########################################################
+#                                                       #
+#               Which Kernel do you have?               #
+#                                                       #
+#########################################################
+
+1) linux
+2) linux-hardened
+3) linux-lts
+4) linux-zen
+Defaults to linux if you choose do not make selection
+"
+echo ""
+read LINUX
+case $LINUX in
+1)
+  sudo pacman -S --noconfirm linux linux-headers;;
+2)
+  sudo pacman -S --noconfirm linux-hardened linux-hardened-headers;;
+3)
+  sudo pacman -S --noconfirm linux-lts linux-lts-headers;;
+4)
+  sudo pacman -S --noconfirm linux-zen linux-zen-headers;;
+*)
+  sudo pacman -S --noconfirm linux linux-headers;;
+esac
+echo ""
+clear
+echo "
+#########################################################
+#                                                       #
+#        Your kernel-headers have been installed        #
+#                                                       #
+#########################################################
+"
 sleep 2
 echo ""
 echo " Install required files "
