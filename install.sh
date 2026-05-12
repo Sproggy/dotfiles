376f8f84f2c787bf571ec615781d9e38e712d0df
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-05-12 11:54:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,15 +30,16 @@ echo "
 #########################################################
 
 1) Arch Linux
-2) Archman Linux
-3) BlueStar Linux
-4) CachyOS
-5) EndeavourOS
-6) Garuda Linux
-7) Kiro Linux (ArcoLinux Rebrand)
-8) Manjaro Linux
-9) My Personal Dotfiles (contains testing features before being promoted to other Dots)
-10) RebornOS 
+2) Archcraft Linux
+3) Archman Linux
+4) BlueStar Linux
+5) CachyOS
+6) EndeavourOS
+7) Garuda Linux
+8) Kiro Linux (ArcoLinux Rebrand)
+9) Manjaro Linux
+10) My Personal Dotfiles (contains testing features before being promoted to other Dots)
+11) RebornOS 
 "
 echo -e "${RESET}"
 echo -e "${CYAN}"
@@ -61,45 +62,50 @@ case $DOTS in
   sh ./1-install.sh;;
 2)
   cd $HOME
+  git clone https://github.com/hyprtk/archcraft-dots.git
+  cd archcraft-dots
+  sh ./1-install.sh;;
+3)
+  cd $HOME
   git clone https://github.com/hyprtk/archman-dots.git
   cd archman-dots
   sh ./1-install.sh;;
-3)
+4)
   cd $HOME
   git clone https://github.com/hyprtk/bslx-dots.git
   cd bslx-dots
   sh ./1-install.sh;;
-4)
+5)
   cd $HOME
   git clone https://github.com/hyprtk/cachy-dots.git
   cd cachy-dots
   sh ./1-install.sh;;
-5)
+6)
   cd $HOME
   git clone https://github.com/hyprtk/endeavour-dots.git
   cd endeavour-dots
   sh ./1-install.sh;;
-6)
+7)
   cd $HOME
   git clone https://github.com/hyprtk/garuda-dots.git
   cd garuda-dots
   sh ./1-install.sh;;
-7)
+8)
   cd $HOME
   git clone https://github.com/hyprtk/kiro-dots.git
   cd kiro-dots
   sh ./1-install.sh;;
-8)
+9)
   cd $HOME
   git clone https://github.com/hyprtk/manjaro-dots.git
   cd manjaro-dots
   sh ./1-install.sh;;
-9)
+10)
   cd $HOME
   git clone https://github.com/hyprtk/my-dots.git
   cd my-dots
   sh ./1-install.sh;;
-10)
+11)
   cd $HOME
   git clone https://github.com/hyprtk/reborn-dots.git
   cd reborn-dots
